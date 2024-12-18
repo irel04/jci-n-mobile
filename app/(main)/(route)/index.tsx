6431caa4816cd3e23e4c8c708d8e0ed854839b13
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2024-12-19 02:33:02 +0800

    feat: apply still on-going component

---
+++ app/(main)/(route)/index.tsx
@@ -0,0 +1,13 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import OnDevelopment from "@/components/OnDevelopment";
+
+const Routes = () => {
+	return (
+		<OnDevelopment/>
+	)
+}
+
+export default Routes
