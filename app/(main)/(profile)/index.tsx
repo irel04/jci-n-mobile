d7c57877fa06be92644f4c78c1e02ef9b8f95675
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2024-12-19 03:23:53 +0800

    feature: profile photo

--- app/(main)/(profile)/index.tsx
+++ app/(main)/(profile)/index.tsx
@@ -1,11 +1,24 @@
 
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 const ProfileTab = () => {
   return (
-	<View>
-	  <Text>ProfileTab</Text>
+	<View className="flex p-2 border-red-500 border-2 h-full">
+	  {/* Profile picture */}
+	  <View className="border-blue-500 border-2 h-10 flex items-center flex-auto gap-1">
+		<Image source={require("@/assets/images/jb-profile.png")} className="rounded-full"/>
+		<Text className="text-h5 font-bold text-brand-900">Jonas Brian Macacua</Text>
+		<Text className="text-caption rounded-lg bg-brand-800 text-white-500 px-[10px] py-[5px]">Employee</Text>
+	  </View>
+	  {/* Personal Information */}
+	  <View className="border-blue-500 border-2 flex flex-auto gap-1">
+		<Text className="text-brand-900 font-bold text-title2">Information</Text>
+		<View className="flex flex-col">
+			
+		</View>
+	  </View>
+	  
 	</View>
   )
 }
