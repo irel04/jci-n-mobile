412d3727db18c23108538b620c491aea462fdf49
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2024-12-19 02:40:03 +0800

    style: adjusted style

--- components/OnDevelopment.tsx
+++ components/OnDevelopment.tsx
@@ -5,11 +5,12 @@ import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons"
 const OnDevelopment = () => {
 	return (
 		<View className="flex items-center h-full justify-center gap-20">
-			<MaterialCommunityIcons name="tools" size={56} color="#85B0F5"/>
+			<MaterialCommunityIcons name="tools" size={56} color="#757576"/>
 			<View className="flex items-center justify-center">
-				<Text className="text-title2 text-neutral-400">Hi! We're still</Text>
-				<Text className="text-h2 font-bold text-brand-200">Developing</Text>
-				<Text className="text-title2 text-neutral-400">We'll lunch this feature very soon</Text>
+				<Text className="text-title2 text-neutral-400">Hi! We're still on</Text>
+				<Text className="text-h2 font-bold text-neutral-500">Development</Text>
+				<Text className="text-title2 text-neutral-400 mt-5">We'll lunch this feature very soon.</Text>
+				<Text className="text-title2 text-neutral-400 mt-2">Stay Tune.</Text>
 			</View>
 		</View>
 	)
