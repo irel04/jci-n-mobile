aa25a1a10143e28c34c5e07579294264cdbbc7df
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2024-12-19 17:22:39 +0800

    feat: make profile detail component

--- app/(main)/(profile)/index.tsx
+++ app/(main)/(profile)/index.tsx
@@ -1,26 +1,36 @@
 
 import { View, Text, Image } from 'react-native'
 import React from 'react'
+import Fontisto from '@expo/vector-icons/Fontisto';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import ProfileDetails from "@/components/profile/ProfileDetails";
 
 const ProfileTab = () => {
-  return (
-	<View className="flex p-2 border-red-500 border-2 h-full">
-	  {/* Profile picture */}
-	  <View className="border-blue-500 border-2 h-10 flex items-center flex-auto gap-1">
-		<Image source={require("@/assets/images/jb-profile.png")} className="rounded-full"/>
-		<Text className="text-h5 font-bold text-brand-900">Jonas Brian Macacua</Text>
-		<Text className="text-caption rounded-lg bg-brand-800 text-white-500 px-[10px] py-[5px]">Employee</Text>
-	  </View>
-	  {/* Personal Information */}
-	  <View className="border-blue-500 border-2 flex flex-auto gap-1">
-		<Text className="text-brand-900 font-bold text-title2">Information</Text>
-		<View className="flex flex-col">
-			
+
+	const profileData = { 
+		name: "Jonas Brian Macacua",
+		birthdate: "December 24, 2001",
+		email: "jonas@gmail.com",
+		phoneNumber: "+63 9704778642",
+		address: "Muslim Compound",
+	}
+
+	return (
+		<View className="flex p-2 border-red-500 border-2 h-full">
+			{/* Profile picture */}
+			<View className="border-blue-500 border-2 h-10 flex items-center flex-auto gap-1">
+				<Image source={require("@/assets/images/jb-profile.png")} className="rounded-full" />
+				<Text className="text-h5 font-bold text-brand-900">Jonas Brian Macacua</Text>
+				<Text className="text-caption rounded-lg bg-brand-800 text-white-500 px-[10px] py-[5px]">Employee</Text>
+			</View>
+
+			{/* Personal Information */}
+			<ProfileDetails data={profileData}/>
+
 		</View>
-	  </View>
-	  
-	</View>
-  )
+	)
 }
 
 export default ProfileTab
---
+++ components/profile/ProfileDetails.tsx
@@ -0,0 +1,47 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Fontisto from "@expo/vector-icons/Fontisto"
+import Ionicons from "@expo/vector-icons/Ionicons"
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons"
+
+interface ProfileProps {
+	data: {
+		name: string
+		birthdate: string
+		email: string
+		phoneNumber: string
+		address: string
+	}
+}
+
+const ProfileDetails = ({ data }: ProfileProps) => {
+	return (
+		<View className="border-blue-500 border-2 flex flex-1 gap-4">
+			<Text className="text-brand-900 font-bold text-title2">Information</Text>
+			<View className="flex flex-col gap-5 p-5 border-neutral-300 border-[1px] rounded-lg ">
+				<View className="flex flex-row items-center gap-1">
+					<Fontisto name="email" size={16} color="#919192" className="flex-none" />
+					<Text className="text-gray-500 flex-1 text-body">Email</Text>
+					<Text className="text-gray-500 text-body">{data.email}</Text>
+				</View>
+				<View className="flex flex-row items-center gap-1">
+					<Ionicons name="phone-portrait-outline" size={16} color="#919192" className="flex-none" />
+					<Text className="text-gray-500 flex-1 text-body">Phone</Text>
+					<Text className="text-gray-500 text-body">{data.phoneNumber}</Text>
+				</View>
+				<View className="flex flex-row items-center gap-1">
+					<SimpleLineIcons name="calendar" size={16} color="#919192" className="flex-none" />
+					<Text className="text-gray-500 flex-1 text-body">BirthDay</Text>
+					<Text className="text-gray-500 text-body">{data.birthdate}</Text>
+				</View>
+				<View className="flex flex-row items-center gap-1">
+					<Ionicons name="location-outline" size={16} color="#919192" className="flex-none" />
+					<Text className="text-gray-500 flex-1 text-body">Address</Text>
+					<Text className="text-gray-500 text-body">{data.address}</Text>
+				</View>
+			</View>
+		</View>
+	)
+}
+
+export default ProfileDetails
