76e2bde7ddc10586d33712bbfb1f7be2f2c3d1ba
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2024-12-20 21:11:52 +0800

    feat: add securetext props

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -9,13 +9,14 @@ interface InputProps {
 	placeholder: string,
 	children?: ReactNode,
 	id: string,
-	value: string
+	value: string,
+	secureTextEntry?: boolean
 }
 
-const Input = ({ children, onChangeText, placeholder, id, value }: InputProps) => {
+const Input = ({ children, onChangeText, placeholder, id, value, secureTextEntry }: InputProps) => {
 	return (
 		<View className="w-72 flex px-5 bg-neutral-100 rounded-2xl flex-row items-center justify-between">
-			<TextInput placeholder={placeholder} onChangeText={onChangeText} id={id} className="w-5/6 text-body" maxLength={25} value={value}/>
+			<TextInput placeholder={placeholder} onChangeText={onChangeText} id={id} className="w-5/6 text-body" maxLength={25} value={value} secureTextEntry={secureTextEntry}/>
 			{/* Pass the icon as */}
 			{children}
 		</View>
