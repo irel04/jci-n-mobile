c6376a37c3bb6535195ab2d42b26755e2e74c995
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-01-05 23:45:02 +0800

    feat: pie chart

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -1,48 +1,18 @@
-import { View, Text, Image, ScrollView } from 'react-native';
-import React from 'react';
-import Weather from '@/components/home/Weather';
-import SolarPower from '@/components/home/SolarPower';
-import TrashBinLevel from '@/components/home/TrashBinLevel';
+import AuthScreen from "@/app/(auth)/index"
 
-const Main = () => {
-	const firstname = "Jonas Brian";
+
+import { View } from "react-native"
+import "../global.css"
+import { useFonts } from "expo-font"
+
+
+const App = () => {
 
 	return (
 		<View className="bg-white-500 h-screen text-base">
-			<View className="flex-1 bg-white">
-				<ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ padding: 16 }}>
-					<View>
-						<Text className="text-left text-h5 font-[700] pb-5">Welcome, {firstname}!</Text>
-					</View>
-
-					<Weather />
-
-					<View className="flex-row gap-[5%] mt-5">
-						<View className="w-[30%]">
-							<SolarPower />
-						</View>
-
-						<View className="flex-col justify-between p-3 rounded-xl bg-brand-700 items-left gap-2 w-[65%]">
-							<Text className="text-white-500">Placeholder</Text>
-						</View>
-					</View>
-
-					<View>
-						<TrashBinLevel />
-					</View>
-
-					<View className="mt-5">
-						<View className="flex-col justify-between border-2 rounded-xl border-brand-700 items-left gap-2 w-full">
-							<Image
-								source={{ uri: 'https://via.placeholder.com/250' }}
-								className="w-full h-[250px] rounded-lg"
-							/>
-						</View>
-					</View>
-				</ScrollView>
-			</View>
+			<AuthScreen />
 		</View>
-	);
-};
+	)
+}
 
-export default Main;
+export default App
