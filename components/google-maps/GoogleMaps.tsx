f8de74d7aa01fb1cd3f69c3a78dc3951c6bb598c
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-01-11 12:12:07 +0800

    chore: development build

--- components/google-maps/GoogleMaps.tsx
+++ components/google-maps/GoogleMaps.tsx
@@ -1,46 +1,58 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { View } from "react-native";
-import mapStyleSheet from "./Style";
+import React, { useEffect, useRef } from 'react';
+import { View } from 'react-native';
+import mapStyleSheet from './Style';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
+
 const INITIAL_REGION = {
-	latitude: 14.5895,
-	longitude: 121.0152,
-	latitudeDelta: 0.0922,
-	longitudeDelta: 0.0421,
-}
+  latitude: 14.5895,
+  longitude: 121.0152,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
 
 const INITIAL_MARKER_POS = {
-	latitude: 14.5895,
-	longitude: 121.0152,
-	title: "BIN-A"
-}
+  latitude: 14.5895,
+  longitude: 121.0152,
+  title: 'BIN-A',
+};
 
 const GoogleMaps = () => {
+  const mapRef = useRef(null);
+  const markerRef = useRef(null);
 
-	const mapRef = useRef(null)
+  const focusMap = () => {
+    const STA_MESA = {
+      latitude: 14.5895,
+      longitude: 121.0152,
+      latitudeDelta: 0.0012,
+      longitudeDelta: 0.0012,
+    };
 
-	const focusMap = () => {
-		const STA_MESA = {
-			latitude: 14.5895,
-			longitude: 121.0152,
-			latitudeDelta: 0.0012,
-			longitudeDelta: 0.0012,
-		}
+    mapRef.current?.animateToRegion(STA_MESA, 1000); // 1000 ms for smooth animation
+  };
 
-		mapRef.current?.animateToRegion(STA_MESA)
+  const focusMarker = () => {
+    const LNGLAT = {
+      latitude: 14.5895,
+      longitude: 121.0152,
+    };
 
-	}
+    if (markerRef.current) {
+      markerRef.current.animateMarkerToCoordinate(LNGLAT, 1000); // Smooth marker movement
+    }
+	
+  };
 
-	useEffect(() => {
-		focusMap()
-	}, [])
+  useEffect(() => {
+    focusMap();
+  }, []);
 
-	return (
-		<MapView style={mapStyleSheet.map} initialRegion={INITIAL_REGION} ref={mapRef} provider={PROVIDER_GOOGLE}>
-			<Marker coordinate={INITIAL_MARKER_POS} />
-		</MapView>
-	)
-}
+  return (
+    <MapView style={mapStyleSheet.map} initialRegion={INITIAL_REGION} ref={mapRef} provider={PROVIDER_GOOGLE}>
+      <Marker coordinate={INITIAL_MARKER_POS} ref={markerRef} />
+    </MapView>
+  );
+};
 
-export default GoogleMaps
+export default GoogleMaps;
