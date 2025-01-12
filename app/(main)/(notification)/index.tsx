cd5568804bfa4489b52e1387aacfa5ff36e392e5
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-01-12 18:43:35 +0800

    feat: notification integration

--- app/(main)/(notification)/index.tsx
+++ app/(main)/(notification)/index.tsx
@@ -1,32 +1,60 @@
 import { Text, View, TouchableOpacity, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NotificationCard from '@/components/notifications/NotificationCard'
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { supabase } from "@/utils/supabase";
 
 export default function NotificationTab() {
     // Notifications data moved here
-    const notifications = {
-        today: [
-            { id: 1, message: 'Bin A is almost full', time: '5 mins ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 2, message: 'Bin B is almost empty', time: '10 mins ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 3, message: 'Bin C is almost full', time: '15 mins ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 4, message: 'Bin D is being cleaned', time: '1 hour ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 5, message: 'Bin E needs repair', time: '2 hours ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 6, message: 'Bin F is full', time: '3 hours ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 7, message: 'Bin G is almost full', time: '4 hours ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 8, message: 'Bin H is being cleaned', time: '5 hours ago', imageUri: 'https://via.placeholder.com/50' },
-        ],
-        last7Days: [
-            { id: 1, message: 'Bin G was emptied', time: '2 days ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 2, message: 'Bin H needs cleaning', time: '3 days ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 3, message: 'Bin I was replaced', time: '5 days ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 4, message: 'Bin J is overflowing', time: '6 days ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 5, message: 'Bin K was cleaned', time: '7 days ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 6, message: 'Bin L was emptied', time: '7 days ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 7, message: 'Bin M needs repair', time: '4 days ago', imageUri: 'https://via.placeholder.com/50' },
-            { id: 8, message: 'Bin N is full', time: '5 days ago', imageUri: 'https://via.placeholder.com/50' },
-        ]
-    }
+    // const notifications = {
+    //     today: [
+    //         { id: 1, message: 'Bin A is almost full', time: '5 mins ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 2, message: 'Bin B is almost empty', time: '10 mins ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 3, message: 'Bin C is almost full', time: '15 mins ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 4, message: 'Bin D is being cleaned', time: '1 hour ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 5, message: 'Bin E needs repair', time: '2 hours ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 6, message: 'Bin F is full', time: '3 hours ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 7, message: 'Bin G is almost full', time: '4 hours ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 8, message: 'Bin H is being cleaned', time: '5 hours ago', imageUri: 'https://via.placeholder.com/50' },
+    //     ],
+    //     last7Days: [
+    //         { id: 1, message: 'Bin G was emptied', time: '2 days ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 2, message: 'Bin H needs cleaning', time: '3 days ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 3, message: 'Bin I was replaced', time: '5 days ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 4, message: 'Bin J is overflowing', time: '6 days ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 5, message: 'Bin K was cleaned', time: '7 days ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 6, message: 'Bin L was emptied', time: '7 days ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 7, message: 'Bin M needs repair', time: '4 days ago', imageUri: 'https://via.placeholder.com/50' },
+    //         { id: 8, message: 'Bin N is full', time: '5 days ago', imageUri: 'https://via.placeholder.com/50' },
+    //     ]
+    // }
+
+
+    const [upComingNotifications, setUpComingNotifications] = useState([])
+
+    // Initial fetching of notification to load 
+    useEffect(() => {
+
+        const fetchNotifications = async () => {
+            try {
+                const { data: userAuth } = await supabase.auth.getUser()
+
+                if(!userAuth.user) return
+
+                const { data: user } = await supabase.from("users_details").select("id").eq("auth_id", userAuth.user.id)
+
+                const { data: notifications } = await supabase.from("notifications").select(`notification_type, bins(color, set), created_at`).eq("nearest_user_id", user[0].id)
+
+                setUpComingNotifications(notifications)
+
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        fetchNotifications()
+
+    }, [])
 
     return (
         <View className="p-5">
@@ -42,7 +70,7 @@ export default function NotificationTab() {
             <ScrollView showsVerticalScrollIndicator={false} className='h-[95%]'>
                 <View>
                     {/* Pass the notifications data as props to NotificationCard */}
-                    <NotificationCard notifications={notifications} />
+                    <NotificationCard notifications={upComingNotifications} />
                 </View>
             </ScrollView>
         </View>
--- components/notifications/NotificationCard.tsx
+++ components/notifications/NotificationCard.tsx
@@ -1,24 +1,46 @@
 import { Text, View, Image, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
+import { timeAgo } from "@/utils/helper"
 
-export default function NotificationCard({ notifications }) {
+
+interface Bins {
+    color: string,
+    set: string,
+}
+
+interface NotificationObj {
+    notification_type: string,
+    created_at: string,
+    bins: Bins
+}
+
+interface NotificationCardAttr {
+    notifications: NotificationObj[]
+}
+
+export default function NotificationCard({ notifications }: NotificationCardAttr) {
     // State to toggle the visibility of all notifications for Today and Last 7 Days
     const [showAllToday, setShowAllToday] = useState(false)
     const [showAllLast7Days, setShowAllLast7Days] = useState(false)
 
     // Function to render notifications with pagination (show first 5 and then full list)
-    const renderNotifications = (notifications, showAll) => {
-        const displayedNotifications = showAll ? notifications : notifications.slice(0, 5)
+    const renderNotifications = (notifications: any, showAll: any) => {
+        const displayedNotifications = showAll ? notifications : notifications.slice(0, 10)
 
-        return displayedNotifications.map((notification) => (
-            <View key={notification.id} className="flex-row items-center mt-2">
+        return displayedNotifications.map((notification: NotificationObj, index: number) => (
+            <View key={index} className="flex-row items-center mt-2 py-2">
                 <Image
-                    source={{ uri: notification.imageUri }}
-                    className="w-12 h-12 rounded-full mr-4"
+                    source={require("@/assets/images/pup-logo.png")}
+                    className="w-14 h-14 rounded-full mr-4"
                 />
                 <View className="flex-1">
-                    <Text className="text-body">{notification.message}</Text>
-                    <Text className="text-caption text-gray-500">{notification.time}</Text>
+                    <Text className="text-[14px]">
+                        <Text className="font-bold">{notification.bins.set}</Text>
+                        <Text className="font-bold"> {notification.bins.color}</Text>
+                        <Text> is</Text>
+                        <Text className="font-bold"> {notification.notification_type}</Text>
+                    </Text>
+                    <Text className="text-caption text-gray-500">{timeAgo(notification.created_at)}</Text>
                 </View>
             </View>
         ))
@@ -29,17 +51,17 @@ export default function NotificationCard({ notifications }) {
             {/* Today's Notifications */}
             <View className="relative mb-5">
                 <View className="flex-row justify-between items-center">
-                    <Text className="text-lg font-bold">Today</Text>
+                    <Text className="text-lg font-bold">Earlier</Text>
                 </View>
-                {renderNotifications(notifications.today, showAllToday)}
+                {renderNotifications(notifications, showAllToday)}
 
-                <TouchableOpacity onPress={() => setShowAllToday(!showAllToday)}>
+                {notifications.length > 10 && <TouchableOpacity onPress={() => setShowAllToday(!showAllToday)}>
                     <Text className="text-brand-300 text-body absolute right-[2%] underline">{showAllToday ? 'See Less' : 'See All'}</Text>
-                </TouchableOpacity>
+                </TouchableOpacity>}
             </View>
 
             {/* Last 7 Days Notifications */}
-            <View className="mt-6 relative">
+            {/* <View className="mt-6 relative">
                 <View className="flex-row justify-between items-center">
                     <Text className="text-lg font-bold">Last 7 Days</Text>
                 </View>
@@ -48,7 +70,7 @@ export default function NotificationCard({ notifications }) {
                 <TouchableOpacity onPress={() => setShowAllLast7Days(!showAllLast7Days)}>
                     <Text className="text-brand-300 text-body absolute right-[2%] underline">{showAllLast7Days ? 'See Less' : 'See All'}</Text>
                 </TouchableOpacity>
-            </View>
+            </View> */}
         </View>
     )
 }
