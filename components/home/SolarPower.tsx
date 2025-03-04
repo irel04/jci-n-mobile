7f833fe44144cb41b83bbaacd910bd27325fa217
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-03-04 20:36:27 +0800

    feat(home): add trash bin touchable opacity

--- components/home/SolarPower.tsx
+++ components/home/SolarPower.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function SolarPower() {
+
+    const handleOnTrashBinPress = async () => {
+        console.log("hello")
+    }
+
     return (
         <View className='flex-col justify-between mt-5 rounded-xl items-center'>
-            <Image source={require('@/assets/images/trashbin.png')} className='w-[67.93px] h-[100px] items-center mb-5' />
+            <TouchableOpacity onPress={handleOnTrashBinPress}>
+                <Image source={require('@/assets/images/trashbin.png')} className='w-[67.93px] h-[100px] items-center mb-5' />
+            </TouchableOpacity>
 
             <View className='flex-col justify-between p-3 rounded-xl bg-brand-700 items-left gap-2 w-full'>
                 <Text className='text-title2 text-white-500 font-sans font-bold'>Solar Power</Text>
