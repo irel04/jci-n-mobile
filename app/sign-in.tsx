f6f74c800f4fa5575005830d859794c4c5f563c9
Irel Bacolod <116436224+irel04@users.noreply.github.com>
2025-03-05 16:11:27 +0800

    feat(sign-in): add registration button

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -39,14 +39,14 @@ const Signin = () => {
 		<View className="bg-white-500 text-base h-screen">
 			<View className="flex flex-col h-screen">
 				<KeyboardAvoidingView className="flex-col justify-center items-center pt-5" behavior={Platform.OS === "ios" ? "padding" : "height"}>
-					<View className="flex-auto flex-col justify-center items-center w-full">
+					<View className="flex-col justify-center items-center w-full">
 						<Image source={require("@/assets/images/pup-logo.png")} className="flex-none" />
 						<Text className="text-h3 font-bold text-brand-700 mt-32">Welcome Back</Text>
 						<Text className="text-body">It's nice to see you again! Please enter your details</Text>
 					</View>
 
 					{/* Form */}
-					<View className="flex-auto flex-col gap-5 w-full items-center justify-center h-72" >
+					<View className="flex flex-col gap-5 w-max  items-center justify-center h-72" >
 						<Input onChangeText={(email) => setValue({ ...value, email })} placeholder="Email" id="email" value={value.email}>
 							<FontAwesome name="user-o" size={13} color="#757576" />
 						</Input>
@@ -55,17 +55,24 @@ const Signin = () => {
 								<Feather size={13} name={isPasswordHidden ? 'eye-off' : 'eye'} color="#757576" />
 							</TouchableOpacity>
 						</Input>
+						<Pressable className="self-end" onPress={() => console.log("hello")}>
+							<Text className="underline text-xs text-neutral-500">Forgot Password</Text>
+						</Pressable>
 					</View>
 				</KeyboardAvoidingView>
 				{/* Button */}
-				<View className="justify-center items-center flex-auto h-1/6 gap-5 px-5">
+				<View className="justify-center items-center  gap-4">
 					<CustomButton onPress={handlePressContinue}  styleType={StyleType.BRAND_PRIMARY}>
 						<Text className="text-white-500">{isLoading ? "Please wait..." : "Continue"} </Text>
 						{!isLoading && <AntDesign name="arrowright" size={16} color="white" />}
 					</CustomButton>
-					<Pressable>
-						<Text className="underline text-body">Forgot Password</Text>
-					</Pressable>
+					
+					<View className="flex flex-row gap-1">
+						<Text className="text-sm text-neutral-500">Don't have account? </Text>
+						<Pressable>
+							<Text className="text-sm text-brand-300 underline">Register</Text>
+						</Pressable>
+					</View>
 				</View>
 				<View className="justify-center items-center flex-auto h-1/6 gap-5 px-5">
 					<Text className="text-caption text-neutral-500 text-center">This is a mobile application developed for a research project titled "Enhancing Garbage Management: A Mobile-Integrated, Solar-Powered Waste Collection System."</Text>
